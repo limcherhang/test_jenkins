b04b49050bb52d277fa3255b1e9f81d28b08111c
Aaron-艾倫 <aaron5139@ny.corp>
2023-01-30 16:50:12 +0800

    01-30;16:50

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ def division(x, y):
 
 x = 1
 y = 2
+print(f'{x} + {y} = {add(x, y)}')
+print(f'{x} - {y} = {minus(x, y)}')
+print(f'{x} * {y} = {time(x, y)}')
+print(f'{x} / {y} = {division(x, y)}')
 logging.info(f'{x} + {y} = {add(x, y)}')
 logging.info(f'{x} - {y} = {minus(x, y)}')
 logging.info(f'{x} * {y} = {time(x, y)}')
