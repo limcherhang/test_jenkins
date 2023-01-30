fc690b3c9c43845e3756ce2dc74749cda2ed343a
Aaron-艾倫 <aaron5139@ny.corp>
2023-01-30 16:47:46 +0800

    01-30;16:45

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import logging
+
+logger = logging.getLogger(__name__)
 def add(x, y):
     return x+y
 
@@ -12,7 +15,7 @@ def division(x, y):
 
 x = 1
 y = 2
-print(f'{x} + {y} = ',add(x, y))
-print(f'{x} - {y} = ',minus(x, y))
-print(f'{x} * {y} = ',time(x, y))
-print(f'{x} / {y} = ',division(x, y))
+logging.info(f'{x} + {y} = {add(x, y)}')
+logging.info(f'{x} - {y} = {minus(x, y)}')
+logging.info(f'{x} * {y} = {time(x, y)}')
+logging.info(f'{x} / {y} = {division(x, y)}')
