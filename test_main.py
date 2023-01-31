8c82cfd7bffa2a17ed4e337a8f244443ec3e8d54
Aaron-艾倫 <aaron5139@ny.corp>
2023-01-31 09:50:54 +0800

    01-31;09:50

--- test_main.py
+++ test_main.py
@@ -1,15 +1,15 @@
 from main import *
 import pytest
 
-@pytest.fixture(autouse=True)
+@pytest.fixture()
 def x():
     return 2
 
-@pytest.fixture(autouse=True)
+@pytest.fixture()
 def y():
     return 1
 
-def test_ops():
+def test_ops(x, y):
     assert add(x,y) == 3
     assert minus(x,y) == 1
     assert time(x,y) == 2
